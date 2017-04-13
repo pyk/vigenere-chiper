3f352bec300f76fa05df20faab11e307bdbca242
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2017-04-13 23:49:08 +0700

    vigenere: Add unit test

---
+++ vigenere_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+import vigenere
+
+data = [
+        {
+            'P': 'The imitation game',
+            'K': 'cipher',
+            'C': 'VPTPQZVIIPSEIIBL'
+        }
+    ]
+
+class TestVigenere(unittest.TestCase):
+    def test_enkripsi(self):
+        for d in data:
+            chiper_teks = vigenere.enkripsi(P=d['P'], K=d['K'])
+            self.assertEqual(chiper_teks, d['C'])
+    def test_dekripsi(self):
+        for d in data:
+            plain_teks = vigenere.dekripsi(C=d['C'], K=d['K'])
+            self.assertEqual(plain_teks, d['P'].replace(' ', '').lower())
+
+if __name__ == '__main__':
+    unittest.main()

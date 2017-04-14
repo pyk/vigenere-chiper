6b7fc0353ebbab3a65732bee75fdb87174f3aa84
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2017-04-14 13:52:14 +0700

    telegram_bot: Add interface to set Vigenere key

--- telegram_bot.py
+++ telegram_bot.py
@@ -32,6 +32,21 @@ def get_vigenere_key(user_id):
     key = rds.get(redis_key)
     return key
 
+def set_vigenere_key(user_id, key):
+    redis_key = 'vigenere_key/{}'.format(user_id)
+    key = rds.set(redis_key, key)
+    return key
+
+def set_chat_status(chat_id, status):
+    redis_key = 'vigenere_status/{}'.format(chat_id)
+    # Status ini akan otomatis kehapus jika udah lebih dari 60s
+    rds.setex(redis_key, status, 60)
+
+def get_chat_status(chat_id):
+    redis_key = 'vigenere_status/{}'.format(chat_id)
+    return rds.get(redis_key)
+
+
 # Telegram bot handler
 def handler(message):
     # Answer inline query
@@ -53,16 +68,39 @@ def handler(message):
             bot.answerInlineQuery(query_id, [], 
                         switch_pm_text='Tentukan kunci Vigénere cipher',
                         switch_pm_parameter='setkunci')
-    elif 'text' in message:
+    elif 'entities' in message:
         message_text = message['text']
         if message_text == '/start setkunci':
             chat_id = message['chat']['id']
             message_id = message['message_id']
+            first_name = message['from']['first_name']
             # Answer dengan masukkan kunci
-            bot.sendMessage(chat_id, 
-                    'OK. Kirimkan kunci Vigénere cipher nya ke aku boz',
-                    reply_to_message_id=message_id)
+            pesan = "Hey {}, kirimkan kunci Vigénere cipher nya ke aku ya :). Kuncinya harus huruf abjad aja ya, tanpa spasi, nomor dan simbol-simbol.".format(first_name)
+            bot.sendMessage(chat_id, pesan)
+            set_chat_status(chat_id, 'menunggu_kunci')
             print("DEBUG: menunggu reply kunci vigenere chipernya")
+    elif 'text' in message:
+        # Cek statusnya
+        chat_id = message['chat']['id']
+        chat_status = get_chat_status(chat_id)
+        user_id = message['from']['id']
+        first_name = message['from']['first_name']
+        # Mengatur kuncinya
+        if chat_status == 'menunggu_kunci':
+            kunci = message['text']
+            # Check kuncinya
+            if kunci.isalpha():
+                set_vigenere_key(user_id, kunci)
+                pesan = "Nice! Kunci Vigenere ciphernya berhasil diatur ke: *{}*".format(key)
+                bot.sendMessage(chat_id, pesan, parse_mode='Markdown')
+                set_chat_status(chat_id, 'normal')
+            else:
+                # Ingatkan jika kuncinya tidak valid
+                pesan = "Hey {}, kunci yang kamu kirimkan tidak valid. Pastikan hanya huruf abjad aja ya, tanpa spasi, nomor dan simbol-simbol.".format(first_name)
+                bot.sendMessage(chat_id, pesan)
+                set_chat_status(chat_id, 'menunggu_kunci')
+
+
     print("DEBUG: message:", message)
 
 bot.message_loop(handler, source=update_queue)
